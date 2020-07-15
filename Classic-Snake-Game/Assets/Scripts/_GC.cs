6bfacfb32635d9f5ac2fc31e0b7bcfb4503eb1cd
Wesley-Oliveira <wesley0leby@gmail.com>
2020-07-15 15:38:01 -0300

    Adding movement

--- Classic-Snake-Game/Assets/Scripts/_GC.cs
+++ Classic-Snake-Game/Assets/Scripts/_GC.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class _GC : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum Direction
     {
-        
+        LEFT, UP, DOWN, RIGHT
     }
 
-    // Update is called once per frame
+    public Direction moveDirection;
+    public float delayStep;
+    public float step;
+
+    public Transform head;
+
+    void Start()
+    {
+        StartCoroutine(MoveSnake());
+    }
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            moveDirection = Direction.UP;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            moveDirection = Direction.LEFT;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            moveDirection = Direction.DOWN;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            moveDirection = Direction.RIGHT;
+        }
+    }
+
+    IEnumerator MoveSnake()
+    {
+        yield return new WaitForSeconds(delayStep);
+
+        Vector3 nexPos = Vector3.zero;
+        switch(moveDirection)
+        {
+            case Direction.DOWN:
+                nexPos = Vector3.down;
+                break;
+            case Direction.LEFT:
+                nexPos = Vector3.left;
+                break;
+            case Direction.RIGHT:
+                nexPos = Vector3.right;
+                break;
+            case Direction.UP:
+                nexPos = Vector3.up;
+                break;
+        }
+
+        nexPos *= step;
+        head.position += nexPos;
+
+        StartCoroutine(MoveSnake());
     }
 }
